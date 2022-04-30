816e89cfa6bf285db82bdc8d6d964b0e76701ff3
kubaniekuba <62377130+kubaniekuba@users.noreply.github.com>
2022-04-30 20:28:03 +0200

    Calculator3.1

--- main.py
+++ main.py
@@ -1,71 +1,90 @@
-import math
-def basescore():
+import json
 
-    #Stale z wartoscia dla poszczegolnych miar
-    AV = [0.2, 0.55, 0.62, 0.85]
-    AC = [0.77, 0.44]
-    PR = [0.85, 0.62, 0.27]
-    PRC = [0.85, 0.62, 0.27]
-    UI = [0.85, 0.62]
-    CIA = [0, 0.22, 0.56]
+baseS = ['LOW','MEDIUM', 'HIGH', 'CRITICAL']
+mid=0
+high=0
+vh=0
+CVSS =["attackVector",
+          "attackComplexity",
+          "privilegesRequired",
+          "userInteraction",
+          "scope",
+          "confidentialityImpact" ,
+          "integrityImpact",
+          "availabilityImpact" ,]
+AV = ['PHYSICAL', 'LOCAL', 'ADJACENT', 'NETWORK']
+AC = ['LOW', 'HIGH']
+PR = ['NONE', 'LOW', 'HIGH']
+UI = ['NONE', 'REQUIRED']
+S = ['CHANGED', 'UNCHANGED']
+C = ['NONE', 'LOW', 'HIGH']
+I = ['NONE', 'LOW', 'HIGH']
+A = ['NONE', 'LOW', 'HIGH']
 
-    #Obliczamy ISS ze wzoru 1 - [ (1 - Confidentiality) × (1 - Integrity) × (1 - Availability) ]
-    b = (1 - ((1 - CIA[int(input("Choose value of Confidentiality \n 0 - None \n 1 - Low \n 2 - High"))]) *
-              (1 - CIA[int(input("Choose value of Integrity  \n 0 - None \n 1 - Low \n 2 - High"))]) *
-              (1 - CIA[int(input("Choose value of Availabity  \n 0 - None \n 1 - Low \n 2 - High"))])))
+listy = [AV, AC,PR,UI,S, C, I, A]
 
-    # Obliczamy Impact z podzialem na Unchanged i Changed Scope
-    c = int(input("Choose scope \n 0 - UnChanged \n 1 - Changed"))
-    if c == 0:
-        d = b * 6.42
-        # d to impact dla Unchanged 6.42 × ISS
+c = int(input("Choose what you want to search from the database "
+              "\n 0 - the frequency of occurrence of a given classification category"
+              "\n 1 - frequency of occurrence of the given value on a scale of 0 to 10 "
+              "\n 2 - frequency of occurrence of a given flag "))
+if c == 0:
+    d = int(input("select a classification category: "
+              "\n 0 - Low"
+              "\n 1 -Medium"
+              "\n 2 -High"
+              "\n 3 -Critical"))
+elif c == 1:
+    e = int(input("Select start values on a scale of 0 to 10 "))
+    f = int(input("Select end values on a scale of 0 to 10 "))
 
-        e = (8.22 * (
-            (AV[int(input("Choose value of Attack Vector \n 0 - Physical \n 1 - Local \n 2 - Adjacent \n 3 - Network"))] *
-             (AC[int(input("Choose value of Attack Complexity  \n 0 - Low \n 1 - High "))]) *
-             (PRC[int(input("Choose value of Privileges Required  \n 0 - None \n 1 - Low \n 2 - High"))]) *
-             (UI[int(input("Choose value of User Interaction  \n 0 - None \n 1 - Required "))]))))
-        # e odpowiada za Exploitability 8.22 × AttackVector × AttackComplexity × PrivilegesRequired × UserInteraction
+elif c == 2:
+    g  = int(input("Select base score metric "
+                   "\n 0 - attackVector"
+                   "\n 1 - attackComplexity"
+                    "\n2 -privilegesRequired"
+                     "\n3 - userInteraction"
+                     "\n4 - scope"
+          "\n5 - confidentialityImpact" 
+          "\n6 - integrityImpact"
+          "\n7 - availabilityImpact"
+                   ))
+    print(listy[g])
+    h = int(input("select a number starting with 0 to select the measure: "))
 
-        f = min(round((d + e), 10),10)* 10
-        if b == 0:
-            f = 0
-        # f to base score dla Unchanged,
-        # z zalozeniami ze f nie przekroczy 10 pkt oraz jak ISS jest rowne 0 to caly basescore tez sie rowna 0
+a = int(input("select start dates (2002-2022): "))
+b = int(input("select end dates (2002-2022): "))+1
 
-    else:
-        d = (7.52 * (b - 0.029) - 3.25 * (b - 0.02) ** 15)
-        # d to impact dla changed 7.52 × (ISS - 0.029) - 3.25 × (ISS - 0.02)^15
+for rok in range(a,b):
+    with open('json/nvdcve-1.1-'+str(rok)+'.json', 'r', encoding='utf-8') as myfile1:
+        data = json.load(myfile1)
 
-        e = (8.22 * (
-            (AV[int(
-                input("Choose value of Attack Vector \n 0 - Physical \n 1 - Local \n 2 - Adjacent \n 3 - Network"))] *
-             (AC[int(input("Choose value of Attack Complexity  \n 0 - Low \n 1 - High "))]) *
-             (PR[int(input("Choose value of Privileges Required  \n 0 - None \n 1 - Low \n 2 - High"))]) *
-             (UI[int(input("Choose value of User Interaction  \n 0 - None \n 1 - Required "))]))))
-        # e odpowiada za Exploitability 8.22 × AttackVector × AttackComplexity × PrivilegesRequired × UserInteraction
-        print("ISS: ", b, "Impact: ", d, "Explo: ", e)
-        f = min(round(1.08 * (d + e), 10),10) *10
+        for i in range(len(data["CVE_Items"])):
+            try:
 
-        if b == 0:
-            f = 0
-        # f to base score dla changed,
-        # z zalozeniami ze f nie przekroczy 10 pkt oraz jak ISS jest rowne 0 to caly basescore tez sie rowna 0
-
-
-    if 90 <= f < 100:
-        print("Basescore: ", math.ceil(f) /10, " (Critical)")
-    if 70 <= f < 90:
-        print("Basescore: ", math.ceil(f) /10, " (High)")
-    if 40 <= f < 70:
-        print("Basescore: ", math.ceil(f) /10, " (Medium)")
-    if 0 < f < 40:
-        print("Basescore: ", math.ceil(f) /10, " (Low)")
-    elif f == 0:
-        print("Basescore: ", f, " (None)")
-
-    #Wypisujemy CVSS score oraz Rating
+                if c == 0:
+                    if list(data["CVE_Items"][i]["impact"].keys())[0] == "baseMetricV3":
+                        if data["CVE_Items"][i]["impact"]["baseMetricV3"]["cvssV3"]["baseSeverity"] == baseS[d]:
+                            mid += 1
+                if c == 1:
+                    if list(data["CVE_Items"][i]["impact"].keys())[0] == "baseMetricV3":
+                        if e <= data["CVE_Items"][i]["impact"]["baseMetricV3"]["cvssV3"]["baseScore"] <= f:
+                            high += 1
+                if c == 2:
+                    if list(data["CVE_Items"][i]["impact"].keys())[0] == "baseMetricV3":
+                        if data["CVE_Items"][i]["impact"]["baseMetricV3"]["cvssV3"][CVSS[g]] == listy[g][h]:
+                            vh += 1
 
 
-basescore()
+
+
+
+            except IndexError:
+                pass
+if c == 0:
+    print("The number of times a given flag: ", mid)
+elif c == 1:
+    print('How many times a given value occurs on a scale of 0 to 10: ',high)
+else:
+    print('The number of occurrences of a given classification category: ', vh)
+
 
